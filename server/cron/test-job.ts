d8028cff40917d63807478ebf99811c67397ebaf
Anton Andresen <anton_real_madrid@icloud.com>
2025-01-14 19:35:21 +0100

    remove comment

--- server/cron/test-job.ts
+++ server/cron/test-job.ts
@@ -1,8 +1,4 @@
 import { defineCronHandler } from '#nuxt/cron'
-// import { posts } from '~/drizzle/schema'
-// import { and, eq, lt } from 'drizzle-orm'
-// import db from '~/server/utils/db'
-// import { storageService } from '~/server/services/object-storage'
 
 export default defineCronHandler('daily', async () => {
   try {
